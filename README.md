82c2c7b6dacf61610e196d30c72ab6bef12aebe9
rizz007 <rizzsheikh786@gmail.com>
2020-12-14 15:16:03 +0530

    updated readme file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # AngularLaravelSetup
-This project contains Angular 7 and Laravel Setup on shared hosting server .
-For setup every time when you run 'ng build' commnd in angular project (client) copy ./client/dist/client folder 
-and paste that in ./server/public so that all your angular code will come inside larave web app 
-now you can deploy the server folder on any hosting service.
+- This project contains Angular-7 and Laravel-8 Setup on shared hosting server.
+
+- Every time run 'npm run build-dev' to reflect UI changes from angular to laravel (at client side).
+
+- now you can deploy the "server" folder on any hosting service.
+
 Yeah! that’s it.
--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -7,21 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" type="image/x-icon" href="favicon.ico" />
     {{--angular styles begin--}}
-    {{--angular styles end--}}
+		
+		{{--angular styles end--}}
 </head>
 <body>
 <app-root></app-root>
 {{--angular scripts begin--}}
-    <script src="client/runtime-es2015.js" type="module"></script>
-    <script src="client/runtime-es5.js" nomodule defer></script>
-    <script src="client/polyfills-es5.js" nomodule defer></script>
-    <script src="client/polyfills-es2015.js" type="module"></script>
-    <script src="client/styles-es2015.js" type="module"></script>
-    <script src="client/styles-es5.js" nomodule defer></script>
-    <script src="client/vendor-es2015.js" type="module"></script>
-    <script src="client/vendor-es5.js" nomodule defer></script>
-    <script src="client/main-es2015.js" type="module"></script>
-    <script src="client/main-es5.js" nomodule defer></script>
-{{--angular scripts end--}}
+		<script src="client/runtime-es2015.js" type="module"></script>
+		<script src="client/runtime-es5.js" nomodule defer></script>
+		<script src="client/polyfills-es5.js" nomodule defer></script>
+		<script src="client/polyfills-es2015.js" type="module"></script>
+		<script src="client/styles-es2015.js" type="module"></script>
+		<script src="client/styles-es5.js" nomodule defer></script>
+		<script src="client/vendor-es2015.js" type="module"></script>
+		<script src="client/vendor-es5.js" nomodule defer></script>
+		<script src="client/main-es2015.js" type="module"></script>
+		<script src="client/main-es5.js" nomodule defer></script>
+		{{--angular scripts end--}}
 </body>
 </html>
